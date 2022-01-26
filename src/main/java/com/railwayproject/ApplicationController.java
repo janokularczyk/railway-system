fd498b0d65f17f384f9f7cf27a1cb6b629f9af3b
Janek <62039410+janokularczyk@users.noreply.github.com>
2022-01-26 05:54:59 +0100

    Update ApplicationController.java

--- src/main/java/com/railwayproject/ApplicationController.java
+++ src/main/java/com/railwayproject/ApplicationController.java
@@ -6,18 +6,4 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ApplicationController {
 
-    @GetMapping("/information")
-    public String getAboutUsPage() {
-        return "about_us";
-    }
-
-    @GetMapping("/contact")
-    public String getContactPage() {
-        return "contact_us";
-    }
-
-    @GetMapping("/payment")
-    public String getPayment() {
-        return "payment";
-    }
 }
